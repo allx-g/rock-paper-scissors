aa9bed1cb5ebc8828aca35a84cf24d35646cb1f1
Alex <alexg125125@gmail.com>
2023-07-25 18:34:37 -0500

    Create function to play a round

--- game.js
+++ game.js
@@ -17,5 +17,45 @@ function getComputerChoice() {
 
     return choice;
 }
+
+function capitalize(string) {
+    string = string.toLowerCase();
+    const FIRST_INDEX = 0;
+    const SECOND_INDEX = 1;
+
+    let capitalizedString = string.charAt(FIRST_INDEX).toUpperCase() + "" + string.substring(SECOND_INDEX);
+    return capitalizedString;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
     
-console.log(getComputerChoice());
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    }
+    // Player wins
+    else if (checkIfPlayerWins()) {
+        return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    }
+    else if (checkIfComputerWins()) {
+        return `You lose. ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    }
+    else {
+        return "Invalid round";
+    }
+
+    function checkIfPlayerWins() {
+        return (playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper");
+    }
+
+    function checkIfComputerWins() {
+        return (playerSelection == "rock" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "scissors") ||
+            (playerSelection == "scissors" && computerSelection == "rock");
+    }
+}
+
+console.log(playRound("paper", "scissors"));
