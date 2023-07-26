1991710a38566ce78b146997818d4a3b3060dd8c
Alex <alexg125125@gmail.com>
2023-07-25 19:08:01 -0500

    Add functionality to game function
    The game function will run five times and will keep track of the
    winner every round and the overall score.

--- game.js
+++ game.js
@@ -30,20 +30,23 @@ function capitalize(string) {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    let winner;
     
     if (playerSelection == computerSelection) {
-        return "tie";
+        winner = "none";
     }
     // Player wins
     else if (playerWins()) {
-        return "player";
+        winner = "player";
     }
     else if (computerWins()) {
-        return "computer";
+        winner = "computer";
     }
     else {
-        return "Invalid round";
+        winner = "invalid"
     }
+
+    return winner;
 
     function playerWins() {
         return (playerSelection == "rock" && computerSelection == "scissors") ||
@@ -60,11 +63,28 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     // Track player and computer scores
+    let playerScore = 0;
+    let computerScore = 0;
     // Loop 5 times
+    for (let i = 0; i < 5; i++) {
         // Prompt the player to make a choice
+        let playerSelection = prompt("Make your move: Rock, Paper, or Scissors?");
         // Generate a random choice for the computer
+        let computerSelection = getComputerChoice();
         // Play round
+        let winner = playRound(playerSelection, computerSelection);
         // Whoever wins, increment their score by one
+        updateWinnerScore(winner);
         // Show a message telling the player who won the round
-    // Show final scores and determine a winner       
+    }
+    // Show final scores and determine a winner
+    
+    function updateWinnerScore(winner) {
+        switch (winner) {
+            case "player":
+                playerScore++;
+            case "computer":
+                computerScore++;
+        }
+    }
 }
